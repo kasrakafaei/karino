94d8ff2bcb84d8b69c8c6ea30ae870714982871c
kasrakafaei <popokasra50@gmail.com>
2026-08-24 14:09:43 +0330

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// فایل‌های داخل پوشه اصلی پروژه
+app.use(express.static(__dirname));
+
+// صفحه اصلی
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// اجرای سرور
+app.listen(PORT, () => {
+  console.log(`Karino is running on port ${PORT}`);
+});
